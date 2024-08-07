26e2f42eadec00dd92f2f4922e78d101b35f30f6
MostakemHossain <mostakememon123@gmail.com>
2024-08-07 10:44:18 +0600

    initilize the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
