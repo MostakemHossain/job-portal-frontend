738d5d26460a82f7ba2a44f2af91727a2c9259ef
MostakemHossain <mostakememon123@gmail.com>
2024-08-12 13:29:49 +0600

    filter jobs added

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -1,8 +1,46 @@
+import { Label } from "./ui/label";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+
+const filterData = [
+    {
+        filterType: "Location",
+        array: ["USA", "Canada", "UK", "Germany", "Australia", "India", "Japan"]
+    },
+    {
+        filterType: "Industry",
+        array: ["FrontEnd Developer", "Backend Developer", "Full Stack Developer", "Data Scientist", "Product Manager", "Designer", "QA Engineer"]
+    },
+    {
+        filterType: "Salary",
+        array: ["0-30K$", "30-50K$", "50-70K$", "70-90K$", "90-110K$", "110-130K$", "130-150K$"]
+    },
+];
 
 const FilterCard = () => {
     return (
-        <div>FilterCard</div>
-    )
+        <div className="w-full bg-white p-3 rounded-md">
+            <h1 className="font-bold text-lg">Filter Jobs</h1>
+            <hr className="mt-3" />
+            <RadioGroup>
+                {filterData.map((data, index) => (
+                    <div key={index}>
+                        <h2 className="font-bold text-lg">{data.filterType}</h2>
+                        {
+                            data.array.map((item, index) => {
+                                return (
+                                    <div className="flex items-center space-x-2 my-2">
+                                        <RadioGroupItem value={item} />
+                                        <Label>{item}</Label>
+
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                ))}
+            </RadioGroup>
+        </div>
+    );
 }
 
-export default FilterCard
+export default FilterCard;
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,38 +1,35 @@
-import FilterCard from "./FilterCard"
-import Job from "./Job"
-import Navbar from "./Shared/Navbar"
-const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8]
+import FilterCard from "./FilterCard";
+import Job from "./Job";
+import Navbar from "./Shared/Navbar";
+
+const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Jobs = () => {
     return (
         <div>
             <Navbar />
-            <div className="max-w-7xl mx-auto mt-5">
-                <div className="flex gap-5">
-                    <div className="w-20%">
+            <div className="w-full mx-auto px-4 sm:px-6 lg:px-20 mt-10">
+                <div className="flex flex-col lg:flex-row gap-10">
+                    <div className="">
                         <FilterCard />
                     </div>
-                    {
-                        jobsArray.length <= 0 ? <span>No jobs found</span> : (
-                            <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
-                                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                                    {
-                                        jobsArray.map((item, index) => (
-                                            <div>
-                                                <Job />
-                                            </div>
-                                        ))
-                                    }
-
-                                </div>
-
+                    <div className="lg:flex-1 h-[88vh] overflow-y-auto pb-5">
+                        {jobsArray.length <= 0 ? (
+                            <span>No jobs found</span>
+                        ) : (
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                {jobsArray.map((item, index) => (
+                                    <div key={index}>
+                                        <Job />
+                                    </div>
+                                ))}
                             </div>
-                        )
-                    }
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Jobs
+export default Jobs;
