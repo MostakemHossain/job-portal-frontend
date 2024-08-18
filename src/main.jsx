c0a8fa4f182dc76896b50567cfd9cb5d77f7adab
MostakemHossain <mostakememon123@gmail.com>
2024-08-18 11:29:27 +0600

    redux persists add

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import {
+  persistStore,
+} from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App.jsx'
 import { Toaster } from './components/ui/sonner.jsx'
 import './index.css'
 import { store } from './redux/store.js'
 
+const persistor = persistStore(store)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <Provider store={store}>
-      <Toaster />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <Toaster />
+        <App />
+
+      </PersistGate>
     </Provider>
 
   </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authSlice from './authSlice'
 import jobSlice from './jobSlice'
 
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+const rootReducer=combineReducers({
+  auth:authSlice,
+  job:jobSlice
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 export const store = configureStore({
-  reducer: {
-   auth: authSlice,
-   job:jobSlice
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
